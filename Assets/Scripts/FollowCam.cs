ddcfe8faea08ba01acfc668ca6346be0996cec0a
Christina Haristes <christina.haristes@hotmail.de>
2015-06-15 16:00:23 +0200

    Shooting and Camera Follow

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCam : MonoBehaviour {
+
+	public static FollowCam S;
+
+	public GameObject poi; //poi=Point of Interest, thing camera has to follow
+
+	private float camZ;
+
+	void Awake() {
+		S = this;
+		camZ = this.transform.position.z;
+	}
+
+	void Update() {
+		if (poi == null) { //null in inspector shown as "none" (because you can't set an object to zero)
+			return;
+		}
+
+		Vector3 destination = poi.transform.position; //.transform.position because you only want the destination to have the 
+		//coordinates as poi. with transform.position it only grabs the coordinates
+
+		destination.z = camZ;
+
+		transform.position = destination;
+	}
+
+}
--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -6,7 +6,7 @@ public class Slingshot : MonoBehaviour {
 
 	// Fields seen in the Inspector panel
 	public GameObject prefabProjectile;
-
+	public float shotMult = 4.0f;
 
 	// Internal variable
 	private GameObject launchPoint;
@@ -70,9 +70,24 @@ public class Slingshot : MonoBehaviour {
 		//Find the difference between launchPos and mouse position
 		Vector3 mouseDelta = mousePos3D - launchPos;
 
+		//MouseDelte states how strong flight of projectile will be, but that it doesn't flies endlessly you 
+		//determine a maximum for MouseDelte, more thatn the maximum is just cut and shrinked to determined Maximum
+		float maxMagnitude = this.GetComponent<SphereCollider> ().radius;
+
+		mouseDelta = Vector3.ClampMagnitude(mouseDelta, maxMagnitude);
+
 		//Move the projectile to this new position
-		projectile.transform.position = mousePos3D;
+		projectile.transform.position = launchPos + mouseDelta;
 
+		if (Input.GetMouseButtonUp (0)) {
+			aimingMode=false;
+			//Gravity is disabled with Kinematic set to false
+			projectile.GetComponent<Rigidbody> ().isKinematic = false;
 
+			projectile.GetComponent<Rigidbody>().velocity = -mouseDelta*shotMult;
+
+			FollowCam.S.poi = projectile;
+
+		}
 	}
 }
