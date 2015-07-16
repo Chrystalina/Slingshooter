f2638c1eaa486dc47176e3a4bcd724bcf708deb7
Christina Haristes <christina.haristes@hotmail.de>
2015-07-16 20:24:27 +0200

    Trying to create an image, with Congratulations, when the goal is hit!

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -6,11 +6,18 @@ public class Goal : MonoBehaviour {
 	// A static field visible from anywhere
 	public static bool goalMet = false;
 	public AudioSource Victory;
+	//public GameObject Congratulations;
+
+
 
 	
 	void Awake(){
 		AudioSource Victory = GetComponent<AudioSource>();
+		//GameObject Congratulations = GetComponent<Congratulations> ();
 		Victory.Pause();
+
+		//Congratulations.RectTransform.Width = 0.0f;
+		//Congratulations.RectTransform.Height = 0.0f;
 	}
 	
 
@@ -24,8 +31,10 @@ public class Goal : MonoBehaviour {
 			c.a = 1.0f;
 			GetComponent<Renderer>().material.color = c;
 			Victory.Play();
-
-
+			//Congratulations.transform.localScale.x = 3.5f;
+			//Congratulations.transform.localScale.y = 3.5f;
+			
+			
 
 
 		}
