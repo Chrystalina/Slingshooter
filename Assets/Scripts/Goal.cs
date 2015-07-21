4cfaed0dfd4964de6f1e3f0d551a3a4851bf2c25
Christina Haristes <christina.haristes@hotmail.de>
2015-07-21 11:48:30 +0200

    Putting in a Star as a goal and trying to add a particle system appearing, when the goal is hit!

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -6,6 +6,7 @@ public class Goal : MonoBehaviour {
 	// A static field visible from anywhere
 	public static bool goalMet = false;
 	public AudioSource Victory;
+	public static ParticleSystem Glow;
 
 
 
@@ -13,9 +14,13 @@ public class Goal : MonoBehaviour {
 	void Awake(){
 		AudioSource Victory = GetComponent<AudioSource>();
 		Victory.Pause();
+		//ParticleSystem Glow = GetComponent<ParticleSystem> ();
+		Goal.Glow.Pause ();
 
 
 	}
+
+	//public void Pause(bool withChildren)=true;
 	
 
 	void OnTriggerEnter(Collider other) {
@@ -25,11 +30,11 @@ public class Goal : MonoBehaviour {
 		if(other.gameObject.tag == "Projectile") {
 			goalMet = true;
 			// Set alpha to higher opacity
-			Color c = GetComponent<Renderer>().material.color;
-			c.a = 1.0f;
-			GetComponent<Renderer>().material.color = c;
+			//Color c = GetComponent<Renderer>().material.color;
+			//c.a = 1.0f;
+			//GetComponent<Renderer>().material.color = c;
 			Victory.Play();
-
+			Goal.Glow.Play();
 			
 
 
